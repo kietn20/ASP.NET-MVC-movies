03893a281ab81273dc5c5267bab29e631f7cc9f7
kietn20 <kietnguyen3698@gmail.com>
2024-11-15 14:31:32 -0800

    first commit

---
+++ MovieList/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.Models
+{
+    public class Movie
+    {
+        // EF will instruct the database to automatically generate this value
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2050, ErrorMessage = "Year must be between 1889 and 2050.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int? Rating { get; set; }
+
+        [Required(ErrorMessage = "Please enter a genre.")]
+        public string GenreId { get; set; } = string.Empty;
+
+        [ValidateNever]
+        public Genre Genre { get; set; } = null!;
+    }
+}
